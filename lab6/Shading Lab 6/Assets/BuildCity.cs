9c898285f086dae9ac466e1e679dd960440caf82
Santiago Ponce <saponce@ucsc.edu>
2020-05-21 13:36:18 -0700

    Circular City

--- lab6/Shading Lab 6/Assets/BuildCity.cs
+++ lab6/Shading Lab 6/Assets/BuildCity.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class BuildCity : MonoBehaviour
 {
     public GameObject[] buildings;
     public int mapWidth = 20;
     public int mapHeight = 20;
+    public int mapRadius = 20;
 
     public int space = 1;
     public float noiseDepth = 10.0f;
@@ -18,6 +20,7 @@ public class BuildCity : MonoBehaviour
         float seed = UnityEngine.Random.Range(0, 100);
 
         //At each location, it will instantiate a random building
+        /*
         for (int h = 0; h < mapHeight; h++)
         {
             for (int w = 0; w < mapWidth; w++)
@@ -40,11 +43,32 @@ public class BuildCity : MonoBehaviour
                     Instantiate(buildings[4], pos, Quaternion.identity);
             }
         }
-    }
+        */
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Circular city
+        for(int r = 0; r < mapRadius; r++)
+        {
+            int h = 0;
+            for(int d = 0; d < 360; d += 5) //Every 5 degrees?
+            {
+                h++;
+                int result = (int)(Mathf.PerlinNoise(r / noiseDepth + seed, h / noiseDepth + seed) * 10);
+
+                Transform placement = gameObject.transform;
+                placement.position = new Vector3(r * space, 0, 0);
+                placement.RotateAround(new Vector3(0, 0, 0), transform.up, d);
+
+                if (result < 2)
+                    Instantiate(buildings[0], placement.position, Quaternion.Euler(0, d, 0));
+                else if (result < 4)
+                    Instantiate(buildings[1], placement.position, Quaternion.Euler(0, d, 0));
+                else if (result < 6)
+                    Instantiate(buildings[2], placement.position, Quaternion.Euler(0, d, 0));
+                else if (result < 8)
+                    Instantiate(buildings[3], placement.position, Quaternion.Euler(0, d, 0));
+                else if (result < 10)
+                    Instantiate(buildings[4], placement.position, Quaternion.Euler(0, d, 0));
+            }
+        }
     }
 }
---
+++ lab6/Shading Lab 6/Assets/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach this script to a GameObject to rotate around the target position.
+public class Rotate : MonoBehaviour
+{
+    GameObject target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Sphere");
+        //gameObject.transform.Rotate(Vector3.up, 30);
+        //transform.position = new Vector3(20, 2, 20);
+    }
+
+    void Update()
+    {
+        // Spin the object around the world origin at 20 degrees/second.
+        gameObject.transform.RotateAround(target.transform.position, transform.up, 30 * Time.deltaTime);
+    }
+}
